008cc2ee1b06696adfb365517273e5682eae6109
潘一波u <37875004+632106444@users.noreply.github.com>
2019-12-22 14:11:16 +0800

    Add files via upload

---
+++ C语言练习记录(持续更新中)/练习/day1vs的配置和使用/day1vs的配置和使用/day1vs.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void)
+{
+
+	printf("潘一波 34期 12号\n");
+	printf("2019年6月25日");
+	printf("hello world!\n");
+	printf("hello world\n");
+	printf("hello world!\n");
+	printf("hello world\n"); 
+	printf("hello world!\n");
+	printf("hello world\n");
+	getchar();
+	printf("hello world!\n");
+	printf("hello world\n");
+	printf("hello world!\n");
+	printf("hello world\n");
+	getchar();
+	printf("hello world!\n");
+	printf("hello world\n");
+	getchar();
+	return 0;
+}
