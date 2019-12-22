19306f2d77468d11e9750e4d2c38b4c4462b4918
潘一波u <37875004+632106444@users.noreply.github.com>
2019-12-22 14:29:03 +0800

    Add files via upload

---
+++ C语言练习记录(持续更新中)/草稿/test4/test4/test3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int fun(int a,int b)
+#define ADD(a,b) £¨a£©+£¨b)
+{
+	return a+b;
+}
+int main(void)
+{
+	int c,d,e;
+	
+	e=fun(3,4);
+	printf("%d\n",e);
+	getchar();
+	getchar();
+	return 0;
+}
