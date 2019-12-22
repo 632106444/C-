92e6a1952ea5bc35cf54c14c97cb5dbee8a7e67a
潘一波u <37875004+632106444@users.noreply.github.com>
2019-12-22 14:21:21 +0800

    Add files via upload

---
+++ C语言练习记录(持续更新中)/草稿/test1/test1/text.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#ifndef  TEXT_H
+#define TEXT_H
+
+
+void TEXT()
+{
+	printf("%s\n","666");
+	printf("%s\n","777");
+	return 0;
+}
+#endif
---
+++ C语言练习记录(持续更新中)/草稿/test1/test1/text1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "text.h"
+int main()
+{
+	TEXT();
+	printf("%s\n","888");
+	getchar();
+	getchar();
+	return 0; 
+
+}
